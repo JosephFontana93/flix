d114b3ed147311c5378ee781efef8c8ea73eb7fe
Joseph Fontana <fontanjo@oregonstate.edu>
2020-09-19 21:21:38 -0700

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Joseph Fontana on 9/19/20.
+//  Copyright © 2020 Joseph Fontana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
